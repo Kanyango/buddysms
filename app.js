dd42c8a4ed112163064ffe647947ba4dd1327b5a
Kanyango <kariukikanyango@gmail.com>
2016-09-17 11:55:48 +0300

    Update app.js

--- app.js
+++ app.js
@@ -38,8 +38,8 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.all('*' ,function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
+app.all('https://buddysms.herokuapp.com*' ,function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "https://buddysms.herokuapp.com*");
     res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
     res.header("Access-Control-Allow-Headers", "accept, content-type, x-parse-application-id, x-parse-rest-api-key, x-parse-session-token");
    if ('OPTIONS' == req.method) {
