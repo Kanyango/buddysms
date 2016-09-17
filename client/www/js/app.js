8931d185c0f7c614bdfce131a157db38ba70863a
Kanyango <kariukikanyango@gmail.com>
2016-09-17 15:15:49 +0300

    Update app.js

--- client/www/js/app.js
+++ client/www/js/app.js
@@ -9,8 +9,9 @@ angular.module('buddySms',
 	])
 .config(function ($httpProvider) {
         $httpProvider.defaults.useXDomain = true;
-        $httpProvider.defaults.headers.post['X-Posted-By'] = 'https://buddysms.herokuapp.com/';
+        $httpProvider.defaults.headers.post['X-Posted-By'] = 'https://buddysms.herokuapp.com';
         $httpProvider.defaults.headers.post["Content-Type"] = "application/json";
+        delete $httpProvider.defaults.headers.common['X-Requested-With'];
     })
 .config(function($stateProvider , $urlRouterProvider){
 
