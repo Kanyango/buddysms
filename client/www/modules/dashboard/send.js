0b384ee5f7d7389e2c946f8e11d7de1a49754aca
Kanyango <kariukikanyango@gmail.com>
2016-09-19 13:05:54 +0300

    Update send.js

--- client/www/modules/dashboard/send.js
+++ client/www/modules/dashboard/send.js
@@ -29,12 +29,11 @@ angular.module('buddySms.send' , ['ngTagsInput','typeahead'])
       
       $scope.testAuth = function()
       {
-        $http.post('https://sms.solutions4mobiles.com/apis/auth', {headers : { 'Content-Type': 'application/json'} ,
-        {
-	           "type"     : "access_token",
-	           "username" : "kariukikanyango@gmail.com",
-	           "password" : "androidapps"
-         }})
+        $http.post('https://sms.solutions4mobiles.com/apis/auth', { headers : { 'Content-Type': 'application/json' ,
+              "type"     : "access_token",
+              "username" : "kariukikanyango@gmail.com",
+              "password" : "androidapps"
+        })
         .then(function(response){
           console.log(response.data);
         });
