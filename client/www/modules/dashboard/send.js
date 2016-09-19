90cbb68ae10f0d25b93a344999b93f0691b09523
Kanyango <kariukikanyango@gmail.com>
2016-09-19 08:59:56 +0300

    Update send.js

--- client/www/modules/dashboard/send.js
+++ client/www/modules/dashboard/send.js
@@ -31,7 +31,7 @@ angular.module('buddySms.send' , ['ngTagsInput','typeahead'])
       $scope.testAuth = function()
       {
       
-        $http.post('https://www.solutions4mobiles.com//apis/auth' , { 
+        $http.post('http://www.solutions4mobiles.com//apis/auth' , { 
 		        "type" : "access_token",
 		        "username" : "Dave",
 		        "password" : "androidapps"
