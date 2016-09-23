0b6a59cb25403b6cb003051197c30f8f450f9aaa
Kanyango <kariukikanyango@gmail.com>
2016-09-23 10:46:09 +0300

    Update app.js

--- app.js
+++ app.js
@@ -25,11 +25,11 @@ require('./models')(app, mongoose);
 app.set('port' , config.port);
 
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET, PUT, POST,DELETE");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-     next();
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
 });
+
 
 
 app.use(express.static(__dirname + '/client/www'));
