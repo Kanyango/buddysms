a7cadd374b5291bad7bf633b14f855fc794047ea
Kanyango <kariukikanyango@gmail.com>
2016-09-24 11:18:57 +0300

    Update message.js

--- server/service/message.js
+++ server/service/message.js
@@ -37,7 +37,7 @@ var message = {
 	  		method: 'POST',
 	  		headers: {
 			        'Content-Type': 'application/json',
-			        'Authorization' : 'Bearer' + 9cd5fd10-646a-4e7d-82a1-eb6aea787154
+			        'Authorization' : 'Bearer 9cd5fd10-646a-4e7d-82a1-eb6aea787154'
 			    },
 	  		json : [{
 	  				"to"      : req.body.rec,
