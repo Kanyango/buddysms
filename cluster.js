6a8cab7d0830a78db02b3083ee21de3b8e05819b
Kanyango <kariukikanyango@gmail.com>
2016-09-27 11:13:02 +0300

    Update cluster.js

--- cluster.js
+++ cluster.js
@@ -1,25 +1,23 @@
-'use strict';
-
 var cluster = require('cluster');
 
-if(cluster.isMaster)
-{
-	var numWorkers = require('os').cpus().length;
+if(cluster.isMaster) {
+    var numWorkers = require('os').cpus().length;
 
-	console.log('Master cluster is running' + numWorkers + 'workers.....');
+    console.log('Master cluster setting up ' + numWorkers + ' workers...');
 
-	for(var i = 0; i< numWorkers ; i++)
-	{
-		cluster.fork();
-	}
-	cluster.on('online' , function(worker){
-		console.log('Worker' + worker.process.id + 'is online' );
-	});
-	cluster.on('exit' , function(worker , code , signal){
-		console.log('Worker' + worker.process.id + ' died with code: ' + code + ', and signal: ' + signal);
-    cluster.fork();
-	});
-}else{
+    for(var i = 0; i < numWorkers; i++) {
+        cluster.fork();
+    }
 
-	require('./app');
-}
+    cluster.on('online', function(worker) {
+        console.log('Worker ' + worker.process.pid + ' is online');
+    });
+
+    cluster.on('exit', function(worker, code, signal) {
+        console.log('Worker ' + worker.process.pid + ' died with code: ' + code + ', and signal: ' + signal);
+        console.log('Starting a new worker');
+        cluster.fork();
+    });
+} else {
+   	require('./app.js');
+    };
