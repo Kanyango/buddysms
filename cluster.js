d3c80722974f31222662f28df42343a500f6317c
Kanyango <kariukikanyango@gmail.com>
2016-09-27 08:46:12 +0300

    Update cluster.js

--- cluster.js
+++ cluster.js
@@ -1,5 +1,6 @@
 var cluster = require('cluster');
 
+
 if(cluster.isMaster)
 {
 	var numWorkers = require('os').cpus().length;
@@ -8,13 +9,19 @@ if(cluster.isMaster)
 
 	for(var i = 0; i< numWorkers ; i++)
 	{
-		cluster.fork();
+		var env = {workerId: i},
+                newWorker = cluster.fork(env);
+                newWorker.process.env = env;
+		//cluster.fork();
 	}
 	cluster.on('online' , function(worker){
 		console.log('Worker' + worker.process.id + 'is online' );
 	});
 	cluster.on('exit' , function(worker , code , signal){
 		console.log('Worker' + worker.process.id + ' died with code: ' + code + ', and signal: ' + signal);
+		var env = worker.process.env,
+                newWorker = cluster.fork(env);
+                newWorker.process.env = env;
 	});
 }else{
 
