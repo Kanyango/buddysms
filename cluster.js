c664de28c7511a4f59ace0a1ca87a30a5cb59878
Kanyango <kariukikanyango@gmail.com>
2016-09-27 09:03:58 +0300

    Update cluster.js

--- cluster.js
+++ cluster.js
@@ -1,29 +1,23 @@
 var cluster = require('cluster');
 
+if(cluster.isMaster) {
+    var numWorkers = require('os').cpus().length;
 
-if(cluster.isMaster)
-{
-	var numWorkers = require('os').cpus().length;
+    console.log('Master cluster setting up ' + numWorkers + ' workers...');
 
-	console.log('Master cluster is running' + numWorkers + 'workers.....');
+    for(var i = 0; i < numWorkers; i++) {
+        cluster.fork();
+    }
 
-	for(var i = 0; i< numWorkers ; i++)
-	{
-		var env = {workerId: i},
-                newWorker = cluster.fork(env);
-                newWorker.process.env = env;
-		//cluster.fork();
-	}
-	cluster.on('online' , function(worker){
-		console.log('Worker' + worker.process.id + 'is online' );
-	});
-	cluster.on('exit' , function(worker , code , signal){
-		console.log('Worker' + worker.process.id + ' died with code: ' + code + ', and signal: ' + signal);
-		var env = worker.process.env,
-                newWorker = cluster.fork(env);
-                newWorker.process.env = env;
-	});
-}else{
+    cluster.on('online', function(worker) {
+        console.log('Worker ' + worker.process.pid + ' is online');
+    });
 
-	require('./app.js');
+    cluster.on('exit', function(worker, code, signal) {
+        console.log('Worker ' + worker.process.pid + ' died with code: ' + code + ', and signal: ' + signal);
+        console.log('Starting a new worker');
+        cluster.fork();
+    });
+} else {
+    require('/app');
 }
