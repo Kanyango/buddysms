0b39575426367a54024cfd3eb0293b03e993e3a4
Kanyango <kariukikanyango@gmail.com>
2016-09-28 14:22:14 +0300

    Update trans.js

--- server/schema/trans.js
+++ server/schema/trans.js
@@ -3,12 +3,12 @@
 module.exports = function(app , mongoose){
 
 	var transSchema = new mongoose.Schema({
-		
-        user        : {type : mongoose.Schema.Types.ObjectId, ref: 'User'},
-		date        : {type : Date , default: Date.now()},
-		item        : {type : String},
-		amount      : {type : String},
-		qtty        : {type : String}
+	
+		trans_ref    : {type :String},
+		trans_time  : {type : Date},
+		trans_sender_phone   : {type : String},
+		amount        : {type : String},
+		currency      : {type: String}
 		
 	});
 	app.db.model('Trans' , transSchema);
